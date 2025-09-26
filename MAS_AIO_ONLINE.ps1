d90f80ede6787b8cd64edd784deb9ccf29abd6f6
apistech <apistech@gmail.com>
2025-09-26 15:41:29 +0700

    Create MAS_AIO_ONLINE.ps1

---
+++ MAS_AIO_ONLINE.ps1
@@ -0,0 +1,9 @@
+# ============================
+# Microsoft Activation Scripts (MAS)
+# ============================
+
+Write-Host "Jika PowerShell diblokir, jalankan: Set-ExecutionPolicy Unrestricted"
+Write-Host "Setelah selesai, kunci kembali dengan: Set-ExecutionPolicy Restricted"
+Write-Host ""
+
+irm https://get.activated.win | iex
